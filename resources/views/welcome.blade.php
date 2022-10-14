011e122149aee9b0a19f2a926b887ed886f3d955
minhtc <minhtc@newwave.vn>
2022-10-14 16:49:10 +0700

    add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>user-profile.nws-dev.com</title>
+        <link rel="icon" type="image/x-icon" href="/newwave.jpg">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
     </head>
     <body class="antialiased">
         <div id="app"></div>
